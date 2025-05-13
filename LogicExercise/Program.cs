aeada2b40b8ba013cea2feb20dfc4cc7679ded5a
RavenFajar <ravenfajar2502@gmail.com>
2025-05-13 11:26:08 +0700

    Day1LogicExercise

---
+++ LogicExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1 ; i <= x  ; i++){
+    if(i%3 == 0 && i%5 == 0){
+        Console.WriteLine("foobar");
+        continue;
+    }else if(i%3 == 0){
+        Console.WriteLine("foo");
+        continue;
+    }else if(i%5 == 0){
+        Console.WriteLine("bar");
+        continue;
+    }else{
+    Console.WriteLine(i);
+    }
+}
