230ca42bf0f7e619d045046ba959bcd90062a811
RavenFajar <ravenfajar2502@gmail.com>
2025-05-13 11:28:12 +0700

    Day1LogicExercise

--- LogicExercise/Program.cs
+++ LogicExercise/Program.cs
@@ -4,13 +4,10 @@ int x = Convert.ToInt32(Console.ReadLine());
 for(int i = 1 ; i <= x  ; i++){
     if(i%3 == 0 && i%5 == 0){
         Console.WriteLine("foobar");
-        continue;
     }else if(i%3 == 0){
         Console.WriteLine("foo");
-        continue;
     }else if(i%5 == 0){
         Console.WriteLine("bar");
-        continue;
     }else{
     Console.WriteLine(i);
     }
--- LogicExercise/obj/Debug/net8.0/LogicExercise.AssemblyInfo.cs
+++ LogicExercise/obj/Debug/net8.0/LogicExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae6a02afe6b22db98f1fcf62011003f8d911bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeada2b40b8ba013cea2feb20dfc4cc7679ded5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
