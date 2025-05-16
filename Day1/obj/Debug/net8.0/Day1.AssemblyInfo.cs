1f666e86d38f9e0f9c4e09e49d1c559b872c6754
RavenFajar <ravenfajar2502@gmail.com>
2025-05-16 11:20:48 +0700

    IndividualLearningDay4

--- Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b4a1b94ad40afebcb355bf5991daaf5efe2bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30a06112e68ac96216938cc97fb89266ce58cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LogicExercise/obj/Debug/net8.0/LogicExercise.AssemblyInfo.cs
+++ LogicExercise/obj/Debug/net8.0/LogicExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b4a1b94ad40afebcb355bf5991daaf5efe2bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30a06112e68ac96216938cc97fb89266ce58cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
