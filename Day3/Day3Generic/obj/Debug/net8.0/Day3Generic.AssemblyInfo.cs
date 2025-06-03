f6f370c20da0576a298bc9eb49059998273bf2e2
RavenFajar <ravenfajar2502@gmail.com>
2025-06-03 08:30:44 +0700

    FinalLogicChallenge

--- Day3/Day3Generic/obj/Debug/net8.0/Day3Generic.AssemblyInfo.cs
+++ Day3/Day3Generic/obj/Debug/net8.0/Day3Generic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day3Generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d0c5691ec026afb828b906211976d2074269a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91725de525b4cc0b75623a481525bd975c51fec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day3Generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day3Generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FInalLogicTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Security.Claims;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        MyClass program = new MyClass();
+        program.PrintFooBarJazzHuzz(x);
+
+    }
+}
+class MyClass
+{
+    public bool isChecked;
+    public Dictionary<int, string> rules = new Dictionary<int, string>();
+    public MyClass()
+    {
+        AddRules(3, "foo");
+        AddRules(5, "bar");
+        AddRules(7, "jazz");
+        AddRules(9, "huzz");
+    }
+
+    public void AddRules(int number, string rule)
+    {
+        rules.Add(number, rule);
+    }
+
+    public void PrintFooBarJazzHuzz(int x)
+    {
+        for (int i = 1; i <= x; i++)
+        {   
+            isChecked = false;
+            foreach (var rule in rules)
+            {
+                if (i % rule.Key == 0)
+                {
+                    Console.Write(rule.Value);
+                    isChecked = true;
+                }
+            }
+            if (isChecked == false)
+            {
+                Console.Write(i);
+            }
+            Console.Write(", ");
+
+        }
+    }
+}
