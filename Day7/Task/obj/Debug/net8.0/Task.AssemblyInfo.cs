1ade64ac3a9b19bb6e558188d40efde30e4627a7
RavenFajar <ravenfajar2502@gmail.com>
2025-06-03 14:56:51 +0700

    FinalLogicTest

--- Day7/Task/obj/Debug/net8.0/Task.AssemblyInfo.cs
+++ Day7/Task/obj/Debug/net8.0/Task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f370c20da0576a298bc9eb49059998273bf2e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9737532395e1300d60ba4f6e6f6573c77a447f01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
