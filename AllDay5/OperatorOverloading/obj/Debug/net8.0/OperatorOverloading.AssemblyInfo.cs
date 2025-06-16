25535bfb8979ea9ac3668b670949e22e5fb49ccc
RavenFajar <ravenfajar2502@gmail.com>
2025-06-16 10:29:35 +0700

    16 Juni 2025

--- AllDay5/OperatorOverloading/obj/Debug/net8.0/OperatorOverloading.AssemblyInfo.cs
+++ AllDay5/OperatorOverloading/obj/Debug/net8.0/OperatorOverloading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperatorOverloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ad6ee1129a377522f1e37470c8240cbee957cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d0796a7e484831829453b3705e0e2d336996a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperatorOverloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperatorOverloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
